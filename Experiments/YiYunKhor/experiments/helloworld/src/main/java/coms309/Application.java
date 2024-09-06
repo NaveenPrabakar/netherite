64359bc1ff99b47fe3307e9086ab0f87dcd75154
Annabelle5474 <15776ABk@gmail.com>
2024-09-06 10:23:47 -0500

    just adding smtg

--- Experiments/YiYunKhor/experiments/helloworld/src/main/java/coms309/Application.java
+++ Experiments/YiYunKhor/experiments/helloworld/src/main/java/coms309/Application.java
@@ -13,7 +13,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Application {
 	
     public static void main(String[] args) throws Exception {
-        SpringApplication.run(Application.class, args);
+        SpringApplication.run(Application.class, khor);
     }
 
 }
