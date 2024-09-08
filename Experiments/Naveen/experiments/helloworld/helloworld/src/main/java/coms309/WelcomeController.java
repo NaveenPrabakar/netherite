b4ff2147786814437c89e7e669ee30c02a08a0e2
Naveen Prabakar <nvnprabakar@gmail.com>
2024-09-08 13:52:49 -0500

    Added Experiment one

--- Experiments/Naveen/experiments/helloworld/helloworld/src/main/java/coms309/WelcomeController.java
+++ Experiments/Naveen/experiments/helloworld/helloworld/src/main/java/coms309/WelcomeController.java
@@ -3,17 +3,25 @@ package coms309;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.stereotype.Controller;
 
-@RestController
+@Controller
 class WelcomeController {
+
+    /*
+    changes I made:
+    - Used Controller instead of restController to launch the html
+    - Used html & css to better display screen
+    - Changed dependencies in pom.xml to support html indexes
+     */
 
     @GetMapping("/")
     public String welcome() {
-        return "Hello and welcome to COMS 309";
+        return "index"; //displays html template to show case springboot web
     }
 
-    @GetMapping("/{name}")
-    public String welcome(@PathVariable String name) {
-        return "Hello and welcome to COMS 309: " + name;
-    }
+    @GetMapping("/experience")
+    public String wel() {
+        return "work-experience";
+    } //displays experience template
 }
