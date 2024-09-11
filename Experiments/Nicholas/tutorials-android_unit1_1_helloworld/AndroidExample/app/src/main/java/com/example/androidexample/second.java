12e19fb113406e410cef282997b2fd1fd2ae3d5c
Nicholas Wang <nicholaspribadi.1209@gmail.com>
2024-09-11 10:30:09 -0500

    added intent

--- Experiments/Nicholas/tutorials-android_unit1_1_helloworld/AndroidExample/app/src/main/java/com/example/androidexample/second.java
+++ Experiments/Nicholas/tutorials-android_unit1_1_helloworld/AndroidExample/app/src/main/java/com/example/androidexample/second.java
@@ -9,7 +9,7 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class second extends AppCompatActivity {
 
     private TextView messageText;
     private TextView messageText2;// define message textview variable
@@ -21,11 +21,11 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);             // link to Main activity XML
+        setContentView(R.layout.activity_second);             // link to Main activity XML
 
         /* initialize UI elements */
         messageText = findViewById(R.id.main_msg);      // link to message textview in the Main activity XML
-        messageText.setText("Hello World");
+        messageText.setText("Hello Intent");
         messageText2 = findViewById(R.id.textView2);
 
 
@@ -44,12 +44,10 @@ public class MainActivity extends AppCompatActivity {
         switchbutton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this, second.class);
+                Intent intent = new Intent(second.this, MainActivity.class);
                 startActivity(intent);
             }
         });
-
-
 
 
     }
