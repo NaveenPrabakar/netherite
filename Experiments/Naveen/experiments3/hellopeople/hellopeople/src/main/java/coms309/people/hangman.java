8840c6c2a101a645454824f5e7b7b1cbdc2453ae
Naveen Prabakar <nvnprabakar@gmail.com>
2024-09-10 20:28:28 -0500

    Finished Experiment three

--- Experiments/Naveen/experiments3/hellopeople/hellopeople/src/main/java/coms309/people/hangman.java
+++ Experiments/Naveen/experiments3/hellopeople/hellopeople/src/main/java/coms309/people/hangman.java
@@ -1,48 +1,63 @@
 package coms309.people;
 
+import java.util.HashSet;
 import java.util.Random;
+import java.util.Set;
 
 
-public class hangman{
+public class hangman {
 
     private String[] wordbank = {"Software", "Computer Science", "Java", "Python"};
-
-    private String word = "";
-
-    private int incorrect = 0;
-
+    private String word;
+    private Set<Character> correctGuesses = new HashSet<>();
+    private Set<Character> incorrectGuesses = new HashSet<>();
+    private int incorrect;
     private String person;
     private String education;
 
-
-
-    public hangman(String person, String education){
+    public hangman(String person, String education) {
         Random r = new Random();
         int num = r.nextInt(wordbank.length);
         word = wordbank[num];
 
         this.person = person;
         this.education = education;
+        this.incorrect = 0;
     }
 
-    public String picture(){
-
+    public String picture() {
         String s = "";
-        for(int i = 0; i < word.length(); i++){
-            s += "*";
+        for (int i = 0; i < word.length(); i++) {
+            char c = word.charAt(i);
+            if (correctGuesses.contains(c)) {
+                s += c;
+            } else {
+                s += "*";
+            }
         }
         return s;
     }
 
-    public String info(){
-        return "Name: " + person + "\nEducation Level: " + education;
+    public String guess(char letter) {
+        if (word.indexOf(letter) != -1) {
+            correctGuesses.add(letter);
+            if (picture().equals(word)) {
+                return "Congratulations! You've guessed the word: " + word;
+            } else {
+                return "Correct guess! Current word: " + picture();
+            }
+        } else {
+            incorrectGuesses.add(letter);
+            incorrect++;
+            if (incorrect >= 6) {
+                return "Game Over! You've made too many incorrect guesses. The word was: " + word;
+            } else {
+                return "Incorrect guess! Try again. Current word: " + picture();
+            }
+        }
     }
 
-    public String name(){
+    public String name() {
         return person;
     }
-
-
-
-
 }
--- Experiments/Naveen/experiments3/hellopeople/hellopeople/src/main/java/coms309/people/hangrunner.java
+++ Experiments/Naveen/experiments3/hellopeople/hellopeople/src/main/java/coms309/people/hangrunner.java
@@ -3,29 +3,53 @@ package coms309.people;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.PathVariable;
-
+import org.springframework.web.bind.annotation.DeleteMapping;
 import java.util.HashMap;
 import java.util.Set;
 
-public class hangrunner{
+@RestController
+public class hangrunner {
 
     private HashMap<String, hangman> players = new HashMap<>();
 
-
-    //gets a list of all players, playing the game
+    // Gets a list of all players playing the game
     @GetMapping("/StartGame")
     public Set<String> personinfo(){
         return players.keySet();
     }
 
-    //Adds player to the lobby
+    // Adds player to the lobby
     @PostMapping("/StartGame")
     public String createGame(@RequestBody hangman h){
         players.put(h.name(), h);
-        return "Player " + h.name() + " has been added to lobby.";
+        return "Player " + h.name() + " has been added to the lobby.";
     }
+
+    // Guess a letter for a specific player
+    @PostMapping("/GuessLetter/{playerName}")
+    public String guessLetter(@PathVariable String playerName, @RequestBody String letter) {
+        hangman player = players.get(playerName);
+        if (player == null) {
+            return "Player not found!";
+        }
+        if (letter.length() != 1) {
+            return "Please guess one letter at a time.";
+        }
+        char guessedLetter = letter.charAt(0);
+        return player.guess(guessedLetter);
+    }
+
+    @DeleteMapping("/EndGame/{playerName}")
+    public String endGame(@PathVariable String playerName) {
+        hangman player = players.remove(playerName);
+        if (player == null) {
+            return "Player " + playerName + " not found!";
+        }
+        return "Player " + playerName + " has been removed from the game.";
+    }
+
+
+
 }
