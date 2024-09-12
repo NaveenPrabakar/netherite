41d97c7b3340a274ebb0306591d3489d1ea8eb08
Annabelle5474 <15776ABk@gmail.com>
2024-09-11 20:13:03 -0500

    add activity3

---
+++ Experiments/YiYunKhor/experiments/PickUpLine/src/main/java/coms309/PickUpLines/PickUpLine.java
@@ -0,0 +1,30 @@
+package coms309.PickUpLines;
+
+public class PickUpLine {
+
+    private int number;
+    private String line;
+
+    public PickUpLine(int number, String line) {
+        this.number = number;
+        this.line = line;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    public String getLine() {
+        return line;
+    }
+
+    public void setLine(String line) {
+        this.line = line;
+    }
+}
+
+
