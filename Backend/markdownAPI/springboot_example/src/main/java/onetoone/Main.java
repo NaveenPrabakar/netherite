42500b40e1332a2888bdc051e4c088d889201afd
Naveen Prabakar <nvnprabakar@gmail.com>
2024-09-22 11:02:46 -0500

    Starting the markDown API

---
+++ Backend/markdownAPI/springboot_example/src/main/java/onetoone/Main.java
@@ -0,0 +1,32 @@
+package onetoone;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+/**
+ * 
+ * @author Vivek Bengre
+ * 
+ */ 
+
+@SpringBootApplication
+@EnableJpaRepositories
+class Main {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Main.class, args);
+    }
+
+    // Create 3 users with their machines
+    @Bean
+    CommandLineRunner initUser() {
+        return args -> {
+            System.out.println("Hello there")
+
+        };
+    }
+
+}
---
+++ Backend/markdownAPI/springboot_example/src/main/java/onetoone/markdown.java
@@ -0,0 +1,11 @@
+package onetoone;
+
+
+@RestController
+@RequestMapping("/files")
+public class markdown{
+
+    //Main directory for the files
+    private final String location = "uploads/";
+
+}
