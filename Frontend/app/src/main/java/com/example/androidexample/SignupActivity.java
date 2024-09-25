e835a3029f50d87c157e91118810af41d07efd02
Jamey Nguyen <asiandeadly5@gmail.com>
2024-09-25 11:42:07 -0500

    Sending signnup information to volley

--- Frontend/app/src/main/java/com/example/androidexample/SignupActivity.java
+++ Frontend/app/src/main/java/com/example/androidexample/SignupActivity.java
@@ -4,11 +4,21 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.VolleyLog;
+import com.android.volley.toolbox.StringRequest;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class SignupActivity extends AppCompatActivity {
 
@@ -19,6 +29,9 @@ public class SignupActivity extends AppCompatActivity {
     private Button signupButton;        // define signup button variable
     private TextView err_msg;
     private Button back2main;
+
+    private TextView msgResponse;
+    private static final String URL_STRING_REQ = "https://www.google.com";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -74,6 +87,7 @@ public class SignupActivity extends AppCompatActivity {
 
                 if (password.equals(confirm)){
                     Toast.makeText(getApplicationContext(), "Signing up", Toast.LENGTH_LONG).show();
+                    makeStringReq();
                     Intent intent = new Intent(SignupActivity.this, MainActivity.class);
                     intent.putExtra("USERNAME", username);  // key-value to pass to the MainActivity
                     intent.putExtra("PASSWORD", password);  // key-value to pass to the MainActivity
@@ -96,4 +110,45 @@ public class SignupActivity extends AppCompatActivity {
             }
         });
     }
+    private void makeStringReq() {
+
+        StringRequest stringRequest = new StringRequest(
+                Request.Method.GET,
+                URL_STRING_REQ,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        // Handle the successful response here
+                        Log.d("Volley Response", response);
+                        msgResponse.setText(response.toString());
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        // Handle any errors that occur during the request
+                        Log.e("Volley Error", error.toString());
+                    }
+                }
+        ) {
+            @Override
+            public Map<String, String> getHeaders() {
+                Map<String, String> headers = new HashMap<>();
+//                headers.put("Authorization", "Bearer YOUR_ACCESS_TOKEN");
+//                headers.put("Content-Type", "application/json");
+                return headers;
+            }
+
+            @Override
+            protected Map<String, String> getParams() {
+                Map<String, String> params = new HashMap<>();
+//                params.put("param1", "value1");
+//                params.put("param2", "value2");
+                return params;
+            }
+        };
+        // Adding request to request queue
+        VolleySingleton.getInstance(getApplicationContext()).addToRequestQueue(stringRequest);
+    }
+
 }
