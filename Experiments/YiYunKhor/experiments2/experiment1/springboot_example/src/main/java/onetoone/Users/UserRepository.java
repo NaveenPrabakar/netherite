ce1439a1ab6755edf6d52718fd419188112db046
Annabelle5474 <15776ABk@gmail.com>
2024-10-02 14:31:08 -0500

    misc.xml

--- Experiments/YiYunKhor/experiments2/experiment1/springboot_example/src/main/java/onetoone/Users/UserRepository.java
+++ Experiments/YiYunKhor/experiments2/experiment1/springboot_example/src/main/java/onetoone/Users/UserRepository.java
@@ -4,14 +4,16 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.transaction.annotation.Transactional;
 
 /**
- * 
- * @author Vivek Bengre
- * 
- */ 
-
+ * In your UserRepository interface, which extends JpaRepository, you're defining two methods for interacting with the User entity:
+ */
+//
 public interface UserRepository extends JpaRepository<User, Long> {
+
+//    findById(int id): This method will retrieve a User object by its id.
     User findById(int id);
 
+//deleteById(Long id): This method will delete a user by their id. The @Transactional annotation ensures that the delete operation occurs within a transaction.
     @Transactional
     void deleteById(int id);
 }
+
