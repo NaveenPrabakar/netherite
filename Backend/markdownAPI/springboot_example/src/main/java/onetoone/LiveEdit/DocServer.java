b0ffae7fc5f33a445adbb1ecadd53f0ac9442d68
Annabelle5474 <15776ABk@gmail.com>
2024-11-06 14:35:45 -0600

    solving naveen doc server oprional import

--- Backend/markdownAPI/springboot_example/src/main/java/onetoone/LiveEdit/DocServer.java
+++ Backend/markdownAPI/springboot_example/src/main/java/onetoone/LiveEdit/DocServer.java
@@ -7,6 +7,7 @@ import java.nio.file.Paths;
 import java.util.Hashtable;
 import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
+import java.util.Optional;
 
 import jakarta.websocket.OnClose;
 import jakarta.websocket.OnError;
