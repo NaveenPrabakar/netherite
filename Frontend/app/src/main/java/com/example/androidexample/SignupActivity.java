93e83d605c2df7c9fee1020239558ebc2446b05f
Nicholas Wang <nawang2@iastate.edu>
2024-12-11 02:41:48 -0600

    i messed up

--- Frontend/app/src/main/java/com/example/androidexample/SignupActivity.java
+++ Frontend/app/src/main/java/com/example/androidexample/SignupActivity.java
@@ -2,12 +2,14 @@ package com.example.androidexample;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.animation.ObjectAnimator;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -18,6 +20,8 @@ import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.example.androidexample.FileView.MainActivity;
 import com.example.androidexample.Volleys.VolleySingleton;
+import com.google.android.material.button.MaterialButton;
+import com.google.android.material.textfield.TextInputEditText;
 
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -28,14 +32,13 @@ import java.util.Map;
 
 public class SignupActivity extends AppCompatActivity {
 
-    private EditText usernameEditText;  // define username edittext variable
-    private EditText passwordEditText;  // define password edittext variable
-    private EditText emailEditText;     // define email edittext variable
-    private EditText confirmEditText;   // define confirm edittext variable
-    private Button loginButton;         // define login button variable
-    private Button signupButton;        // define signup button variable
+    private TextInputEditText usernameEditText;  // define username edittext variable
+    private TextInputEditText passwordEditText;  // define password edittext variable
+    private TextInputEditText emailEditText;     // define email edittext variable
+    private TextInputEditText confirmEditText;   // define confirm edittext variable
+    private MaterialButton loginButton;         // define login button variable
+    private MaterialButton signupButton;        // define signup button variable
     private TextView err_msg;
-    private Button back2main;
 
     private TextView msgResponse;
     private static final String URL_JSON_OBJECT = "http://coms-3090-068.class.las.iastate.edu:8080/user/create";
@@ -53,6 +56,16 @@ public class SignupActivity extends AppCompatActivity {
         signupButton = findViewById(R.id.signup_signup_btn);  // link to signup button in the Signup activity XML
         err_msg = findViewById(R.id.err_msg);
 
+        TextView loginTitle = findViewById(R.id.loginTitle);
+        ImageView appLogo = findViewById(R.id.appLogo);
+
+        ObjectAnimator fadeIn = ObjectAnimator.ofFloat(loginTitle, "alpha", 0f, 1f);
+        fadeIn.setDuration(2000);
+        fadeIn.start();
+
+        appLogo.setScaleX(0.0f);
+        appLogo.setScaleY(0.0f);
+        appLogo.animate().scaleX(1.0f).scaleY(1.0f).setDuration(1500).start();
 
         /* click listener on login button pressed */
         loginButton.setOnClickListener(new View.OnClickListener() {
@@ -113,16 +126,6 @@ public class SignupActivity extends AppCompatActivity {
             }
         });
 
-        back2main = findViewById(R.id.back2main);
-        back2main.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                /* when signup button is pressed, use intent to switch to Signup Activity */
-                Intent intent = new Intent(SignupActivity.this, MainActivity.class);
-                startActivity(intent);  // go to SignupActivity
-            }
-        });
     }
 
     private void makeJsonObjPost(String username, String email, String password) {
@@ -185,3 +188,4 @@ public class SignupActivity extends AppCompatActivity {
         VolleySingleton.getInstance(getApplicationContext()).addToRequestQueue(jsonObjPost);
     };
 }
+
